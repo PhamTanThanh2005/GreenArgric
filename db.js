2b5566d52d68d5154779b35716d5aa41fe72f06c
PhamTanThanh2005 <tanthanhquanghai@gmail.com>
2026-03-29 16:30:48 +0700

    Update for local running

--- db.js
+++ db.js
@@ -1,14 +1,33 @@
+// import sql from "mssql"
+
+// const config = {
+//     user: "hieu",
+//     password: "hieu",
+//     server: "127.0.0.1",
+//     port: 1433,
+//     database: "green-farm",
+//     options: {
+//         encrypt: false,
+//         trustServerCertificate: true
+//     }
+// }
+
+// const pool = await sql.connect(config)
+
+// export default pool
+// export { sql }
+
 import sql from "mssql"
 
 const config = {
     user: "hieu",
     password: "hieu",
-    server: "127.0.0.1",
-    port: 1433,
+    server: "localhost",
     database: "green-farm",
     options: {
         encrypt: false,
-        trustServerCertificate: true
+        trustServerCertificate: true,
+        instanceName: "SQLEXPRESS"
     }
 }
 
